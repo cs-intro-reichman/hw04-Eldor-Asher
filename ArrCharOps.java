31abc9cba194d76be758b6af9083219fc676a939
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-11-19 09:46:32 +0200

    ArrCharOps.java

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -190,6 +190,10 @@ public class ArrCharOps {
     public static int compareTo(String str1, String str2) {
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
+        if (str1 == null || str2 == null)
+        {
+            return -2;
+        }
         if (str1.equals(str2))
         {
             return 0;
@@ -217,6 +221,7 @@ public class ArrCharOps {
         {
             for (int i=0; i<str1.length();i++)
             {
+                
                 if (str1.charAt(i) > str2.charAt(i))
                 {
                     return 1;
@@ -228,7 +233,8 @@ public class ArrCharOps {
                 else if (i == (str1.length() -1)) 
                 {
                     return -1;
-                } 
+                }
+
             }
         }
         return -2;
--- Primes.java
+++ Primes.java
@@ -1,5 +1,47 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int main_number = Integer.parseInt(args[0]);
+        int index = 2;
+        boolean flag = true;
+        int counter = 0;
+        System.out.println("Prime numbers up to " +main_number+ ":");
+        while (flag);
+        {
+            if (main_number == index) 
+            {
+                flag = false;
+            }
+            if (Is_Prime(index)) 
+            {
+                System.out.println(index);
+                counter ++;
+            }
+            index++;
+        }
+        System.out.println("There are " +counter+ "primes between 2 and " +main_number);
     }
+    public static boolean Is_Prime (int number_check)
+    {
+        int index_check = 2;
+        boolean flag = true;
+        while (flag) 
+        {
+            if (number_check == 2)
+            {
+                flag = false;
+            }
+            
+            if (number_check % index_check == 0)
+            {
+                flag = true;
+            }
+            index_check++;
+            if (number_check == index_check)
+            {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
 }
